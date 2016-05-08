e29c6712147a462ef60dd843f93fd0ed085f4eee
Devesh Agrawal <dagrawal1096@gmail.com>
2016-05-08 21:02:50 +0530

    extension contents

---
+++ extension/main.js
@@ -0,0 +1,35 @@
+/*var opt = {
+                  type: "basic",
+                  title: "Primary Title",
+                  message: "Primary message to display",
+                  iconUrl: "Drawing.png"
+        }*/
+function share_gp() {
+    var list1 = $('.dropdown-menu ul');
+    list1.each(function (i, dropper) {
+        try {
+            if (dropper.children.length === 8 || dropper.children.length === 6) {
+                console.log(dropper);
+                var share_button = $(dropper).prepend('<li> <button id="link" type="button" class="dropdown-link fb_tweet">Share on Google Plus</button> </li>');
+                share_button = share_button.find('.fb_tweet');
+                share_button.click(function () {
+                    var post = $(this).parents().eq(7).find('.tweet-timestamp').attr("href");
+                    url = 'https://twitter.com' + post;
+                    console.log(url);
+                    window.open('https://plus.google.com/share?url=' + url);
+                });
+            }
+             
+            /*chrome.notifications.create(opt,callback);
+            function callback(){
+                console.log("Hello");
+            }*/
+        }
+        catch (dropper) {
+            console.log('Cant find a place to add button');
+        }
+
+    });
+
+}
+setInterval(share_gp, 5000);
